e44c8adfa39725bb1902697c33180c6995206b4a
Benjamin Saldman <benjaminsaldman@gmail.com>
2024-03-05 00:02:13 +0200

    Ex2 ongoing

---
+++ Demo.cpp
@@ -0,0 +1,46 @@
+/*
+ * Demo program for Exercise 2.
+ * Author: Benjamin Saldman.
+ */
+
+#include "Graph.hpp"
+#include "Algorithms.hpp"
+using ariel::Algorithms;
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ariel::Graph g;
+    // 3x3 matrix that represents a connected graph.
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph); // Load the graph to the object.
+
+    g.printGraph();                                    // Should print: "Graph with 3 vertices and 2 edges."
+    cout << Algorithms::isConnected(g) << endl;        // Should print: "1" (true).
+    cout << Algorithms::shortestPath(g, 0, 2) << endl; // Should print: "2" (the shortest path between 0 and 2).
+    cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "0" (false).
+    cout << Algorithms::isBipartite(g) << endl;        // Should print: "1" (true).
+
+    // 5x5 matrix that represents a non-connected graph with a cycle.
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+
+    g.loadGraph(graph2); // Load the graph to the object.
+
+    g.printGraph();                                    // Should print: "Graph with 5 vertices and 4 edges."
+    cout << Algorithms::isConnected(g) << endl;        // Should print: "0" (false).
+    cout << Algorithms::shortestPath(g, 0, 4) << endl; // Should print: "-1" (there is no path between 0 and 4).
+    cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "1" (true).
+    cout << Algorithms::isBipartite(g) << endl;        // Should print: "0" (false).
+}
---
+++ Makefile
@@ -0,0 +1,30 @@
+#!make -f
+
+CXX=clang++-9 
+CXXFLAGS=-std=c++2a -Werror -Wsign-conversion
+VALGRIND_FLAGS=-v --leak-check=full --show-leak-kinds=all  --error-exitcode=99
+
+SOURCES=Graph.cpp Algorithm.cpp
+OBJECTS=$(subst .cpp,.o,$(SOURCES))
+
+run: demo
+	./$^
+
+demo: Demo.o $(OBJECTS)
+	$(CXX) $(CXXFLAGS) $^ -o demo
+
+test: TestCounter.o Test.o $(OBJECTS)
+	$(CXX) $(CXXFLAGS) $^ -o test
+
+tidy:
+	clang-tidy $(SOURCES) -checks=bugprone-*,clang-analyzer-*,cppcoreguidelines-*,performance-*,portability-*,readability-*,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-cppcoreguidelines-owning-memory --warnings-as-errors=-* --
+
+valgrind: demo test
+	valgrind --tool=memcheck $(VALGRIND_FLAGS) ./demo 2>&1 | { egrep "lost| at " || true; }
+	valgrind --tool=memcheck $(VALGRIND_FLAGS) ./test 2>&1 | { egrep "lost| at " || true; }
+
+%.o: %.cpp
+	$(CXX) $(CXXFLAGS) --compile $< -o $@
+
+clean:
+	rm -f *.o demo test
