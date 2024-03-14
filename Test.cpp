7c232742a06aa8882acc60c0aeed2f8dd5e1748b
Benjamin Saldman <benjaminsaldman@gmail.com>
2024-03-14 21:57:07 +0200

    Ex2 updated to Ex1

---
+++ Test.cpp
@@ -0,0 +1,103 @@
+#include "doctest.h"
+#include "Algorithms.hpp"
+#include "Graph.hpp"
+
+using namespace std;
+
+TEST_CASE("Test isConnected")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isConnected(g) == true);
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::isConnected(g) == false);
+}
+
+TEST_CASE("Test shortestPath")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 2) == "0->1->2");
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 4) == "-1");
+}
+TEST_CASE("Test isContainsCycle")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == false);
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == true);
+}
+TEST_CASE("Test isBipartite")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2}, B={1}");
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::isBipartite(g) == "0");
+
+    vector<vector<int>> graph3 = {
+        {0, 1, 2, 0, 0},
+        {1, 0, 3, 0, 0},
+        {2, 3, 0, 4, 0},
+        {0, 0, 4, 0, 5},
+        {0, 0, 0, 5, 0}};
+    g.loadGraph(graph3);
+    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2, 4}, B={1, 3}");
+}
+TEST_CASE("Test invalid graph")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 2, 0},
+        {1, 0, 3, 0},
+        {2, 3, 0, 4},
+        {0, 0, 4, 0},
+        {0, 0, 0, 5}};
+    CHECK_THROWS(g.loadGraph(graph));
+}
---
+++ TestCounter.cpp
@@ -0,0 +1,36 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+using namespace doctest;
+
+const int MIN_TESTS = 20;
+
+int return_code = -1;
+
+struct ReporterCounter : public ConsoleReporter
+{
+    ReporterCounter(const ContextOptions &input_options)
+        : ConsoleReporter(input_options) {}
+
+    void test_run_end(const TestRunStats &run_stats) override
+    {
+        if (run_stats.numAsserts >= MIN_TESTS)
+        {
+            return_code = 0;
+        }
+        else
+        {
+            std::cout << "Please write at least " << MIN_TESTS << " tests! " << std::endl;
+            return_code = 1;
+        }
+    }
+};
+
+REGISTER_REPORTER("counter", 1, ReporterCounter);
+
+int main(int argc, char **argv)
+{
+    Context context;
+    context.addFilter("reporters", "counter");
+    context.run();
+    return return_code;
+}
